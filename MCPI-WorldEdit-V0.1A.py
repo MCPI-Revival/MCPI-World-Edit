2c614be2216dcde86b275e64980b88f7fc36558d
scratchermatch <60678063+scratchermatch@users.noreply.github.com>
2020-02-19 18:42:54 -0700

    Initial Release
    This version is a pretty limited version of the WorldEdit mod for Minecraft Java Edition.
    This is made for use on the Raspberry Pi, and will work on any model.
    You need to have Python 3.7 installed on your pi as well as Minecraft Pi itself to be able to run this program.
    How To Install:
    Open terminal
    Type "pip3 install pynput" (Without the quotes)
    If you don't have IDLE installed, I recommend that you install it with the command:
     "sudo apt-get install idle-python3.7" (Without quotes)
    You should see IDLE appear in your programming menu. Launch it and open the mod (File, Open), then hit "Run" and then "Run Module"
    Make sure that Minecraft Pi is open first.
    If you don't want to install IDLE, you can launch the mod from terminal.
    First, navigate to where the file is stored. It will probably be in your downloads folder. If so, type:
    "cd /home/pi/Downloads"
    Then to launch the program:
    "python MCPI-WorldEdit-V0.1A.py" (change the version number if you're using a different one)
    Make sure to launch Minecraft Pi first before running the program.

---
+++ MCPI-WorldEdit-V0.1A.py
@@ -0,0 +1,80 @@
+#!/usr/bin/python
+import mcpi.minecraft
+import time
+from pynput.keyboard import Key, Listener
+
+mc = mcpi.minecraft.Minecraft.create()
+worldType = 0
+blockID = 0
+
+def getPos1():
+    global pos1x, pos1y, pos1z
+    pos1x, pos1y, pos1z = mc.player.getPos()
+    pos1x = round(pos1x, 0)
+    pos1y = round(pos1y, 0)
+    pos1z = round(pos1z, 0)
+    mc.postToChat("Pos 1 is set to (" + str(pos1x) + ", " + str(pos1y) + ", " + str(pos1z) + ")")
+
+def getPos2():
+    global pos2x, pos2y, pos2z
+    pos2x, pos2y, pos2z = mc.player.getPos()
+    pos2x = round(pos2x, 0)
+    pos2y = round(pos2y, 0)
+    pos2z = round(pos2z, 0)
+    mc.postToChat("Pos 2 is set to (" + str(pos2x) + ", " + str(pos2y) + ", " + str(pos2z) + ")")
+
+def setBlocks(data = 0):
+    global pos1x, pos1y, pos1z, pos2x, pos2y, pos2z, blockID
+    int(blockID)
+    mc.setBlocks(pos1x - 1, pos1y - 1 ,pos1z - 1, pos2x + 1, pos2y - 1, pos2z + 1, blockID)
+    
+def toggle_world():
+    global worldType
+    worldType += 1
+    if worldType > 3:
+        worldType = 1
+    if worldType == 1:
+        mc.postToChat("Toggled to void world")
+        mc.setBlocks(-128, -64,-128, 128, 30, 128, 0)
+        mc.player.setPos(0,10,0)
+        mc.setBlocks(-10, 0, -10, 10, 0, 10, 1)
+        mc.postToChat("Platform Created At (0, 0). Teleporting you there now.")
+    if worldType == 2:
+        mc.postToChat("Toggled to small superflat")
+        mc.setBlocks(-128, -64, -128, 128, -64, 128, 7)
+        mc.setBlocks(-128, -63, -128, 128, -62, 128, 3)
+        mc.setBlocks(-128, -61, -128, 128, -61, 128, 2)
+        x, y, z = mc.player.getPos()
+        mc.player.setPos = x, -60, z
+    if worldType == 3:
+        mc.postToChat("Toggled to large superflat")
+        mc.setBlocks(-128, -64, -128, 128, -64, 128, 7)
+        mc.setBlocks(-128, -63, -128, 128, -50, 128, 3)
+        mc.setBlocks(-128, -49, -128, 128, -49, 128, 2)
+        x, y, z = mc.player.getPos()
+        mc.player.setPos = x, -48, z
+    return
+
+#This somehow detects key presses, I don't know how.
+def on_press(key):
+    print('{0} pressed'.format(key))
+    
+def on_release(key):
+    print('{0} pressed'.format(key))
+    global blockID
+    if key == Key.up:
+        toggle_world()
+    if key == Key.left:
+        getPos1()
+    if key == Key.right:
+        getPos2()
+    if key == Key.down:
+        blockID = int(input("Choose block ID: "))
+        setBlocks()
+        
+
+# Collect events until released
+with Listener(
+        on_press=on_press,
+        on_release=on_release) as listener:
+    listener.join()
